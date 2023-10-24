3bfc7425796133d7461c448a5a61a55d864fc10c
Omar Meza <proyectos@codicewd.com>
2023-10-24 18:53:14 -0500

    key ec

--- city-pulse-project/city-pulse/src/js/main.js
+++ city-pulse-project/city-pulse/src/js/main.js
@@ -1,8 +1,8 @@
 import { dropDown } from "./aside.js";
 import NewsFetcher from "./product.mjs";
 
-const apiKey = import.meta.env.VITE_APIKEY;
-const country = import.meta.env.VITE_COUNTRY;
+const apiKey = "pub_310972aca62cdce0a07f51c386509267aab51";
+const country = "ec";
 
 const newsFetcher = new NewsFetcher(apiKey, country);
 newsFetcher.fetchNews();
