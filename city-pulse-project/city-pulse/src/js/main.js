9b9700022d392316175a815525cb88b03f984496
Omar Meza <proyectos@codicewd.com>
2023-10-24 12:07:53 -0500

    config .env

--- city-pulse-project/city-pulse/src/js/main.js
+++ city-pulse-project/city-pulse/src/js/main.js
@@ -1,10 +1,8 @@
 import { dropDown } from "./aside";
 import NewsFetcher from "./product.mjs"
 
-// import dotenv from "dotenv"
-// dotenv.config()
-const apiKey = "pub_310972aca62cdce0a07f51c386509267aab51";
-const country = "ec";
+const apiKey = import.meta.env.VITE_APIKEY;
+const country = import.meta.env.VITE_COUNTRY;
 
 const newsFetcher = new NewsFetcher(apiKey, country);
 newsFetcher.fetchNews();
--- city-pulse-project/city-pulse/vite.config.js
+++ city-pulse-project/city-pulse/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
+import dotenv from 'dotenv'
+
+dotenv.config() // load env vars from .env
 
 export default defineConfig({
   root: "src/",
+
 
   build: {
     outDir: "../dist",
@@ -9,4 +13,7 @@ export default defineConfig({
       
     },
   },
+  define: {
+    'import.meta.env.ENV_VARIABLE': JSON.stringify(process.env.ENV_VARIABLE)
+  }
 });
